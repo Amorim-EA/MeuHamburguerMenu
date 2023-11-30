72dfc586b744b9d1211bc153aed5d3d733e672d0
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-11-30 01:01:16 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const botaoMenu = document.querySelector('.cabecalho_menu');
 const menu = document.querySelector('.menu-lateral');
+const link = document.querySelectorAll('.menu-lateral_link');
 
 botaoMenu.addEventListener('click', () => {
-  menu.classList.toggle('menu-lateral_link--ativo')
-})
+  menu.classList.toggle('menu-lateral--ativo')
+});
+
+link.addEventListener('click', () => {
+  link.classList.toggle('menu-lateral--ativo')
+});
